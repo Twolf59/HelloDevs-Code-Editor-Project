3d45e4585b9c84dff3d0b06345176e87d2b8de08
yNixos <ynixos@outlook.fr>
2021-08-22 15:42:59 +0200

    base ide

--- HelloDevs IDE/Views/MainWindow.axaml.cs
+++ HelloDevs IDE/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace HelloDevs_IDE.Views
@@ -17,6 +19,15 @@ namespace HelloDevs_IDE.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void test(object? sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog  
+            {  
+                InitialDirectory = @"C:\",
+                Title = "Browse Text Files"
+            };
         }
     }
 }
